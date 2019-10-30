74ee1d084bb3c366e248ad938d4639606fc1d80a
papesaer <papepolgamb@gmail.com>
2019-10-30 18:53:47 +0100

    premiere depo

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="assets/css/cours.css">
+<link rel="stylesheet" href="assets/css/style.css">
+    <title>titre</title>
+</head>
+<body class="bg-info">
+
+<div>
+
+
+
+
+
+<?php
+  require_once 'view/heder.php';
+  ?>
+  
+  
+         <div id="principal">
+                 
+        
+    <h1>WELCOM TO YOUR APPLICATION: H-SPLANNING</h1>
+                 <P>H-PLANNING vous permet:</P>
+                 <p>_Mieu gerer votre agenda</p>
+                 <p>_Bien planifier votre RDV</p><br>
+                     <p>OBTION</p><br>
+            <div id="carre">
+                        <table>
+                            <tr>
+                            
+
+	
+                            <td><button type="button" class="btn btn-primary"><a href="view/connection.php">CONNECTION</a></button></td>
+                                <td><button type="button" class="btn btn-primary"><a href="view/formulaire2.php">INSCRIPTION</a></button></td>
+                            </tr>
+                            
+                        </table>
+             </div>
+
+                </div>
+   
+    
+  </div>
+  
+</body>
+</html>
