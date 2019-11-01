31a4b3773af069d521222ae1412e482eec04c953
papesaer <papepolgamb@gmail.com>
2019-11-01 14:20:49 +0100

    deuxieme deuxieme depot

--- index.php
+++ index.php
@@ -1,40 +1,43 @@
 <!doctype html>
 <html lang="fr" xmlns="http://www.w3.org/1999/html">
 <head>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="assets/css/cours.css">
-<link rel="stylesheet" href="assets/css/style.css">
-    <title>titre</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+
+    <title>Hello, world!</title>
+
+<link rel="stylesheet" href="ssets/css/cours.css">
 </head>
-<body class="bg-info">
+<body class="btn-info">
 
-<div>
-
-
-
-
+<div id="page">
 
 <?php
-  require_once 'view/heder.php';
+  require_once 'view/header.php';
+  
   ?>
   
-  
+  <div id="menu1"></div>
+  <div id="menu2"></div>
          <div id="principal">
                  
         
-    <h1>WELCOM TO YOUR APPLICATION: H-SPLANNING</h1>
-                 <P>H-PLANNING vous permet:</P>
+    <br><h2>WELCOM TO YOUR APPLICATION: <br><br><span>H_PLANNING !</span></h2>
+                 <P>Un compte H_PLANNING vous permet de:</P>
                  <p>_Mieu gerer votre agenda</p>
-                 <p>_Bien planifier votre RDV</p><br>
-                     <p>OBTION</p><br>
+                 <p>_Et bien planifier vos Rendez-Vous</p>
+            <br>   
             <div id="carre">
+                        
                         <table>
+                           <p>OBTION:</p>
                             <tr>
-                            
-
-	
-                            <td><button type="button" class="btn btn-primary"><a href="view/connection.php">CONNECTION</a></button></td>
-                                <td><button type="button" class="btn btn-primary"><a href="view/formulaire2.php">INSCRIPTION</a></button></td>
+                                <td><button type="button" class="btn btn-success"><a href="view/connection.php">CONNECTION</a></button></td>
+                                <td><button type="button" class="btn btn-success"><a href="view/formulaire2.php">INSCRIPTION</a></button></td>
                             </tr>
                             
                         </table>
@@ -42,7 +45,9 @@
 
                 </div>
    
-    
+     <?php
+  require_once 'view/footer.php';
+  ?>
   </div>
   
 </body>
